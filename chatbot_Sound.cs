d91153c38ade3b2f621ac8041b6be6b632e1618f
Tokelo Madela <167033053+St10438033@users.noreply.github.com>
2025-05-14 20:36:45 +0000

    Add files via upload
    Files are now separated for better code structure
    Added new function to chatbot to randomizing, memory store and call, and sentiment detection.

---
+++ chatbot_Sound.cs
@@ -0,0 +1,47 @@
+﻿using System.IO;
+using System.Media;
+using System;
+
+namespace myChatBot1
+{
+    public class chatbot_Sound //play greeting sound class
+    {
+
+        public void play_sound()
+        {
+
+            //retrieve the app location
+            string full_location = AppDomain.CurrentDomain.BaseDirectory;
+
+            Console.WriteLine(full_location);
+
+
+            //replace the folders
+            string new_location = full_location.Replace("bin\\Debug\\", "");
+
+            Console.WriteLine(new_location);
+
+
+            //combine both the new location and audio file
+            string full_path = Path.Combine(new_location, "Pro Chat greet 2.wav");
+
+
+            //try play or catch & display error w/o crashing, error handling
+            try
+            {
+                using (SoundPlayer sound = new SoundPlayer(full_path))
+                {
+
+                    sound.Play(); //play function to play obj sound
+                }
+            }
+            catch (Exception error)
+            {
+
+                Console.WriteLine(error.Message);
+
+            } //end of try&catch
+        }
+
+    }   //end of greeting sound class
+}
---
+++ chatbot_logo.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using System.IO;
+using System;
+
+namespace myChatBot1
+{
+    public class chatbot_logo //display logo class
+    {
+        public void logo()
+        {
+
+            //retrieves project path {getter}
+            string path_project = AppDomain.CurrentDomain.BaseDirectory;
+
+            string newPath = path_project.Replace("bin\\Debug\\", "");
+
+            //replaced and combining project with Logo
+            string full_path = Path.Combine(newPath, "Pro Chat LOGO 2.jpg");
+
+            //pixel data
+            Bitmap image = new Bitmap(full_path);
+            image = new Bitmap(image, new Size(110, 110));
+
+
+            //Nest loop to run & print logo height & width
+            //& Convert to ASCII ART
+            for (int height = 0; height < image.Height; height++)
+            {
+
+                for (int width = 0; width < image.Width; width++)
+                {
+                    //color
+                    Color pc = image.GetPixel(width, height);
+                    int color = (pc.R + pc.B + pc.G) / 3;
+
+                    //adding chars to use for printing logo
+                    char asciiArt = color > 200 ? '.' : color > 150 ? '*' : color > 50 ? '#' : '@';
+                    Console.Write(asciiArt);
+
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }//end of display logo class
+}
